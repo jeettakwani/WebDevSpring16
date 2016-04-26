48bbf021bfecd5bef15d67a4e4997ebecb09b8de
jeettakwani <takwani.j@husky.neu.edu>
2016-04-26 01:28:27 -0400

    openshift

--- server.js
+++ server.js
@@ -8,13 +8,19 @@ var passport      = require('passport');
 var cookieParser  = require('cookie-parser');
 var session       = require('express-session');
 
+
+var connectionString = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://127.0.0.1:27017/webDevSp16';
+/*
 var connectionString = 'mongodb://127.0.0.1:27017/webDevSp16';
 
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+    connection_string =
+        process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
-}
+}*/
 
 var db = mongoose.connect(connectionString);
 
